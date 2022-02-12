a9d3f229a1e05655907f61ebc72f2893cbf809bb
Dennis Gillich <dennisgillich@live.de>
2022-02-12 20:08:35 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string firstName    = GetTextInput("Gebe deinen Vornamen ein: ");
+string lastName = GetTextInput("Gebe deinen Nachnamen ein: ");
+
+Console.WriteLine("Hallo {0} {1}!", firstName, lastName);
+
+static string GetTextInput(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+    
+    Console.ReadKey();
+}
+
+
+
+
+
