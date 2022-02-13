61a4a2d044a6f12b5c66d1fb719520d87bc6bdf0
Dennis Gillich <dennisgillich@live.de>
2022-02-13 13:55:48 +0100

    Den zweiten Teil der Aufgabe hinzugefügt. Math.Pi kennengelernt und als Methode implementiert.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 string firstName    = GetTextInput("Gebe deinen Vornamen ein: ");
 string lastName = GetTextInput("Gebe deinen Nachnamen ein: ");
 
-Console.WriteLine("Hallo {0} {1}!", firstName, lastName);
+Console.WriteLine("Hallo {0} {1}!\n\n", firstName, lastName);
 
 static string GetTextInput(string message)
 {
@@ -14,6 +14,20 @@ static string GetTextInput(string message)
     Console.ReadKey();
 }
 
+// Teil 2 der Übungsaufgabe
+Console.Write("Geben Sie einen Radius ein: ");
+double radius = Convert.ToDouble(Console.ReadLine());
+double area = GetCircleArea(radius);
+
+Console.WriteLine("Der Kreis hat einen Radius von  {0}cm und eine Fläche von {1}cm²\n", radius, area );
+
+static double GetCircleArea(double radius)
+{
+    return Math.PI * radius * radius;
+}
+
+Console.WriteLine("Vielen Dank und bis zum nächsten Mal!");
+Console.ReadKey();
 
 
 
